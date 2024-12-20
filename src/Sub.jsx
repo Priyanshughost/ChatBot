ea223296a4f52ad84844f3ecabdb8e36134dc520
Priyanshu Ghosh <111736328+Priyanshughost@users.noreply.github.com>
2024-12-20 11:51:19 +0530

    Update Sub.jsx

--- src/Sub.jsx
+++ src/Sub.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Sub() {
   return (
     <div className='sub'>
-        <img src="/src/book.svg" alt="" />
+        <img src="https://raw.githubusercontent.com/Priyanshughost/ChatBot/main/src/book.svg" alt="" />
         <h4>Maths</h4>
     </div>
   )
