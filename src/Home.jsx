c0cc28a8176f130b99d1a44df7a02915c94eff2b
Priyanshu Ghosh <111736328+Priyanshughost@users.noreply.github.com>
2024-12-20 11:50:08 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home({ onGetStarted }) {
   return (
     <div className="home">
-      <img className="logo2" src="/logo2.png" alt="" />
+      <img className="logo2" src="https://raw.githubusercontent.com/Priyanshughost/ChatBot/main/src/logo2.png" alt="" />
       <button className="get" onClick={onGetStarted}>
         Get Started
       </button>
