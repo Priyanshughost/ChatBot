1b6f8686be1daf95c85a0ea65bcf1ee335818997
Priyanshu Ghosh <111736328+Priyanshughost@users.noreply.github.com>
2025-02-06 21:56:44 +0530

    Update User.jsx

--- src/User.jsx
+++ src/User.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import Text from '/src/Text.jsx'
-function User() {
-    return (
-        <div className="wrap-user">
-            <div className="res">
-                <Text />
-            </div>
-        </div>
-    )
+import React from "react";
+import Text from "/src/Text.jsx";
+
+function User({ message }) {
+  return (
+    <div className="wrap-user">
+      <div className="res">
+        <Text message={message} />
+      </div>
+    </div>
+  );
 }
 
-export default User
+export default User;
