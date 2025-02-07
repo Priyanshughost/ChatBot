4a37fd051d8cf6dac33d65950ea0c4a05e33d9b7
Priyanshu Ghosh <111736328+Priyanshughost@users.noreply.github.com>
2025-02-07 13:25:09 +0530

    Update Sub.jsx

--- src/Sub.jsx
+++ src/Sub.jsx
@@ -1,22 +1,10 @@
-import React from 'react';
+import React from "react";
 
-function Sub({ url }) {
-  if (!url) return null; // Don't render if no URL is provided
-
-  // Extract domain name from URL
-  const getDomainName = (url) => {
-    try {
-      return url.split("//")[1].split("/")[0]; // Extract domain name
-    } catch (error) {
-      return "Unknown Source";
-    }
-  };
-
+function Sub({ siteName }) {
   return (
-    <div className='sub'>
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        <h4>{getDomainName(url)}</h4>
-      </a>
+    <div className="sub">
+      <img src="https://raw.githubusercontent.com/Priyanshughost/ChatBot/main/src/book.svg" alt="" />
+      <h4>{siteName}</h4>
     </div>
   );
 }
