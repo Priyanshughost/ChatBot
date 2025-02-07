004c673c6636109ff2d5e010bd9c740734ee16e3
Priyanshu Ghosh <111736328+Priyanshughost@users.noreply.github.com>
2025-02-07 12:45:03 +0530

    Update Sub.jsx

--- src/Sub.jsx
+++ src/Sub.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
 
-function Sub() {
+function Sub({ title, url }) {
   return (
-    <div className='sub'>
-        <img src="https://raw.githubusercontent.com/Priyanshughost/ChatBot/main/src/book.svg" alt="" />
-        <h4>Maths</h4>
+    <div className="sub">
+      <img
+        src="https://raw.githubusercontent.com/Priyanshughost/ChatBot/main/src/book.svg"
+        alt="Website Icon"
+      />
+      <h4>
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          {title}
+        </a>
+      </h4>
     </div>
-  )
+  );
 }
 
-export default Sub
+export default Sub;
